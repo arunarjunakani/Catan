0e74bb71b5441ee1a46deb6ecb155ee675b62015
Arun Arjunakani <arunarjunakani@gmail.com>
2018-08-08 09:40:35 -0500

    Rotated hexagons

--- src/Canvas.js
+++ src/Canvas.js
@@ -11,24 +11,45 @@ class Canvas extends Component {
 
   updateCanvas() {
     const ctx = this.refs.canvas.getContext('2d');
-		this.drawHexagon(ctx, 400, this.calcApothem(50), 50, "#FF0000");
-		this.drawHexagon(ctx, 400, 3 * this.calcApothem(50), 50, "#00FF00");
-		this.drawHexagon(ctx, 475, 2 * this.calcApothem(50), 50, "#0000FF");
+		this.drawTiltedHexagon(ctx, 400, 100, 50, "#0000FF");
+		ctx.rotate(Math.PI / 4);
   }
 
-  //Calculates the apothem of a polygon based on the radius
+  //Calculates the apothem of a hexagon based on the radius
   calcApothem(rad) {
   	return rad * Math.sin(Math.PI / 3);
   }
 
-  drawHexagon(ctx, x, y, size, color) {
+  drawHexagon(ctx, x, y, radius, color) {
   	if (!color) {
   		color = "#000000";
   	}
 		ctx.beginPath();
 		for (let i = 0; i < 7; i++) {
-  		ctx.lineTo(x + size * Math.cos(i * 2 * Math.PI / 6), y + size * Math.sin(i * 2 * Math.PI / 6));
+  		ctx.lineTo(x + radius * Math.cos(i * 2 * Math.PI / 6), y + radius * Math.sin(i * 2 * Math.PI / 6));
 		}
+		ctx.strokeStyle = "#000000";
+		ctx.stroke();
+		ctx.fillStyle = color;
+		ctx.fill();
+  }
+
+  drawTiltedHexagon(ctx, x, y, radius, color) {
+  	if (!color) {
+  		color = "#000000";
+  	}
+		ctx.beginPath();
+		for (let i = 0; i < 7; i++) {
+			let coordinates = [x + radius * Math.cos(i * 2 * Math.PI / 6), y + radius * Math.sin(i * 2 * Math.PI / 6)];
+
+			//Rotates the coordinates by 30 degrees
+			let xDelta = coordinates[0] - x;
+			let yDelta = coordinates[1] - y;
+			let rotatedCoordinates = [xDelta * Math.cos(Math.PI / 6) - yDelta * Math.sin(Math.PI / 6) + x, 
+				yDelta * Math.cos(Math.PI / 6) + xDelta * Math.sin(Math.PI / 6) + y];
+			ctx.lineTo(rotatedCoordinates[0], rotatedCoordinates[1]);
+		}
+		ctx.strokeStyle = "#000000";
 		ctx.stroke();
 		ctx.fillStyle = color;
 		ctx.fill();
